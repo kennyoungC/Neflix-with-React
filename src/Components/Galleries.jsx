432d6aff5ceed315886df332e3fa16a039860bbb
Kenneth <obikenneth913@gmail.com>
2022-04-25 17:19:50 +0300

    movie galleries added

---
+++ src/Components/Galleries.jsx
@@ -0,0 +1,10 @@
+import MovieGallery from "./MovieGallery"
+
+const Galleries = () => (
+  <>
+    <MovieGallery movieTitle="harry potter" />
+    <MovieGallery movieTitle="game of thrones" />
+    <MovieGallery movieTitle="merlin" />
+  </>
+)
+export default Galleries
