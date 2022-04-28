dcfb99e50ffcee463e64c7d3ccf8a55d4094ebbc
Kenneth <obikenneth913@gmail.com>
2022-04-28 20:10:45 +0300

    tv shows component added

---
+++ src/Components/TvShowComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import MovieGallery from "./MovieGallery"
+const TvShowComponent = () => {
+  return (
+    <div>
+      <MovieGallery movieTitle="lord of the rings" />
+    </div>
+  )
+}
+
+export default TvShowComponent
